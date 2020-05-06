8f36c7060bd85cef55c108202e05aadb9e13c4e1
Raman Kalita <raman_kalita@epam.com>
2020-05-06 23:50:08 +0300

    try

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ import Period from '../Period';
 
 export default () => (
     <Router>
+        <div>Hello World!</div>
         <div className="app" >
             <Login />
             {/* <AllData /> */}
--- src/components/Variables/Variables.jsx
+++ src/components/Variables/Variables.jsx
@@ -3,5 +3,6 @@ import React from 'react';
 export default () => (
     <div className="variables">
         <p>{`API_SERVER: ${process.env.API_SERVER}`}</p>
+        <p>{`NODE_ENV: ${process.env.NODE_ENV}`}</p>
     </div>
 );
