baa19ff9144f9e0e7c229d87a8991e432da34d31
Raman Kalita <raman_kalita@epam.com>
2020-05-07 23:15:20 +0300

    try travis ci

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ import Period from '../Period';
 
 export default () => (
     <Router>
+        <div>Master branch</div>
         <div>Hello World!</div>
         <div className="app" >
             <Login />
